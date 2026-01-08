a9469e706b3c0adeb41722ba0fa82eafbea7a377
abcoathup <andrew@coathup.net>
2026-01-08 16:52:06 +1100

    readd search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ethereal.news",
-  integrations: [sitemap(), mdx(),],
+  integrations: [sitemap(), mdx(), pagefind()],
   vite: {
     plugins: [tailwindcss()],
   },
