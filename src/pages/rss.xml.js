4212bea22919b882260c6b4ebfb5a0fc30547a41
abcoathup <andrew@coathup.net>
2026-01-20 11:38:42 +1100

    add item image to rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -18,6 +18,7 @@ export async function GET(context) {
     site: context.site,
     xmlns: {
       atom: "http://www.w3.org/2005/Atom",
+      media: "http://search.yahoo.com/mrss/",
     },
     customData: `<atom:link href="${rssUrl}" rel="self" type="application/rss+xml"/>
 <image>
@@ -25,11 +26,17 @@ export async function GET(context) {
 <title>${SITE.TITLE}</title>
 <link>${context.site}</link>
 </image>`,
-    items: items.map((item) => ({
-      title: item.data.title,
-      description: item.data.description,
-      pubDate: item.data.date,
-      link: `/${item.id.replace(/\/index\.md$/, "").replace(/\.md$/, "")}/`,
-    })),
+    items: items.map((item) => {
+      const itemImageUrl = new URL("/etherealnews.jpg", context.site).toString();
+      return {
+        title: item.data.title,
+        description: item.data.description,
+        pubDate: item.data.date,
+        link: `/${item.id.replace(/\/index\.md$/, "").replace(/\.md$/, "")}/`,
+        customData: `<media:content url="${itemImageUrl}" type="image/jpeg">
+<media:description>${SITE.TITLE}</media:description>
+</media:content>`,
+      };
+    }),
   });
 }
